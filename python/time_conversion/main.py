a69a4a4dc333756d5187367aed69b53d4ef1a833
sean-halpin <pulpfictionost@gmail.com>
2024-05-23 18:48:38 +0100

    Hackerrank Python

--- python/time_conversion/main.py
+++ python/time_conversion/main.py
@@ -15,7 +15,14 @@ import sys
 
 def timeConversion(s):
     print(s)
-    return s
+    time, period = s[:8], s[8:]
+    print(time, period)
+    hh, mm, ss = map(int, time.split(':'))
+    if period == 'PM' and hh != 12:
+        hh +=12
+    if period == 'AM' and hh == 12:
+        hh = 0
+    return f'{hh:02d}:{mm:02d}:{ss:02d}'
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
