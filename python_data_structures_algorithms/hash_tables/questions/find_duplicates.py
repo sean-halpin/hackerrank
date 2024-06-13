a4a7d0a49ff43de81eead40efb8dc2e4036f5287
sean-halpin <pulpfictionost@gmail.com>
2024-06-13 22:37:06 +0100

    Data Structures and Algorithms

---
+++ python_data_structures_algorithms/hash_tables/questions/find_duplicates.py
@@ -0,0 +1,31 @@
+def find_duplicates(l):
+    dupes = []
+    seen = {}
+    for c in l:
+        if c in seen:
+            dupes.append(c)
+        else:
+            seen[c] = True
+    return dupes
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
+
