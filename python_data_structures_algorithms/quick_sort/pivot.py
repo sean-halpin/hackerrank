e120542677ad185bb2edf27c16a47bed2859c420
sean-halpin <pulpfictionost@gmail.com>
2024-06-17 20:51:35 +0100

    Data Structures and Algorithms

---
+++ python_data_structures_algorithms/quick_sort/pivot.py
@@ -0,0 +1,44 @@
+def swap(my_list, index1, index2):
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+
+
+def pivot(l, pivot_idx, right):
+    swap = pivot_idx
+    for i in range(pivot_idx+1, right+1):
+        if l[i] < l[swap]:
+            swap+=1
+            swap(l, swap, i)
+    swap(l, pivot_idx, swap)
+    return swap
+
+
+my_list = [4,6,1,7,3,2,5]
+
+print('List before running pivot():')
+print(my_list)
+
+returned_pivot_index = pivot(my_list, 0, 6)
+
+print('\nList after running pivot():')
+print(my_list)
+
+print('\nReturned Swap Index:')
+print(returned_pivot_index)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    List before running pivot():
+    [4, 6, 1, 7, 3, 2, 5]
+
+    List after running pivot():
+    [2, 1, 3, 4, 6, 7, 5]
+
+    Returned Swap Index:
+    3
+
+ """
