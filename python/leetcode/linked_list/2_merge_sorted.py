90992a8452de09a57f235cb453c61afad02227b0
sean-halpin <pulpfictionost@gmail.com>
2024-08-21 16:40:49 +0200

    Hackerrank, LinkedList, Merge Sorted

--- python/leetcode/linked_list/2_merge_sorted.py
+++ python/leetcode/linked_list/2_merge_sorted.py
@@ -3,6 +3,7 @@
 from typing import Optional
 
 
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -10,21 +11,25 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        curr = dummy
-        
+        head = ListNode()
+        tail = head
+
         while list1 and list2:
             if list1.val < list2.val:
-                curr.next = list1
+                tail.next = list1
+                tail = tail.next
                 list1 = list1.next
             else:
-                curr.next = list2
+                tail.next = list2
+                tail = tail.next
                 list2 = list2.next
-            curr = curr.next
-        
-        curr.next = list1 or list2
-        
-        return dummy.next
+
+        if not list1:
+            tail.next = list2
+        elif not list2:
+            tail.next = list1
+
+        return head.next
     
 if __name__ == "__main__":
     sol = Solution()
